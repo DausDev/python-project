e573ad115b0a8273f3e580a385b1fc3a29bcf41a
DausDev <firdaushamsuddin.fs@gmail.com>
2026-03-02 12:05:51 +0800

    First Commit

---
+++ py/01.py
@@ -0,0 +1,13 @@
+name = "daus" #String
+age = 39 #Interger
+weight = 70.5 #Float
+is_male = True #Boolean
+her_name = "leha"
+random_number = None #NoneType
+rabbit = "animal"
+
+
+print(name)
+print(type(age))
+print(her_name)
+print(type(random_number))
---
+++ py/02.py
@@ -0,0 +1,8 @@
+
+x = 2
+y = 5
+
+print ("tambah =" , (x + y))
+print ("darab =", (x * y))
+print ("bahagi =",(x / y))
+print ("tolak =",(x - y))
