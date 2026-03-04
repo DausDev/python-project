fc81e3a18e7db2402454e7b25cf92e9e2848f259
DausDev <firdaushamsuddin.fs@gmail.com>
2026-03-04 12:27:05 +0800

    today Im late to class

---
+++ py/late_join.py
@@ -0,0 +1,45 @@
+# today i joined late, so i will write the code for the previous class, which is about (06-list,07-tuples,08-sets,09-dictionaries) from the pdf.
+# 06-list
+# fruits = ["apple", "banana", "orange"]
+# numbers = [1,2,3,4,5]
+# mixed = ["hello", 42, 3.14, True]
+# empty_list = []
+
+# fruits.append("grape") #add an element to the end of the list
+# fruits.insert(1, "kiwi") #add an element at a specific index
+# fruits.remove("banana") #remove an element by value
+# popped = fruits.pop() #remove and return the last element of the list
+# fruits.sort() #sort the list in ascending order
+# fruits.reverse() #reverse the order of the list
+
+
+# print(fruits[1]) #accessing list element; index starts at 0, so fruits[1] is "banana"
+# print(fruits[-1]) #accessing last element of the list, which is "orange"
+# print(numbers[1:4]) #slicing the list, which gives us [2,3,4]
+# print(numbers[:3]) #slicing from the beginning to index 3 (exclusive), which gives us [1,2,3]
+# print(numbers[2:]) #slicing from index 2 to the end, which gives us [3,4,5]
+
+# # 06-list operation: CRUD a list
+
+# #list operations
+# len(fruits) #get the length of the list
+# "apple" in fruits #check membership in the list, returns True if "apple" is in fruits
+
+# JUMP TO WHERE THE CLASS IS CURRRENTLY AT, WHICH IS 09-dictionaries
+# 09-dictionaries
+
+student = {
+    "name": "Firdaus",
+    "age": 39,
+    "grade": "A",
+    "courses": ["Python", "LINUX", "micro python"]
+}
+
+# Accessing and modifying
+
+student["grade"] = "C"  # Update grade
+student["study method"] = "online bootcamp- jomhack" # Add new key-value pair
+
+print(student["name"])  # Output: Firdaus
+print(student.get("courses")) # Output: ['Python', 'LINUX', 'micro python']
+print(student ["study method"]) # This will raise a KeyError since "study method" does not exist in the dictionary
--- py/loop.py
+++ py/loop.py
@@ -2,6 +2,10 @@
 #     for ii in range(1,6):
 #         print(f"{i} x {ii} = {i*ii}")
 
+
+for i in range(1,11):
+    for ii in range(1,3):
+        print(f"{i} x {ii} = {ii*i}")
 # for i in range (2,21):
 #     is_prime = True
 #     for j in range(2, int(i**0.5) + 1):
@@ -13,13 +17,5 @@
 #     else:
 #         print(f"{i} is not a prime number.")
 
-for i in range (2,21):
-    prime_num = True
-    for ii in range (2,i):
-        if i % ii == 0:
-            prime_num = False
-            break
-    if prime_num:
-            print(f"{i} is a PRIME NUMBER.")
     # else:
     #         print(f"{i} is NOT a PRIME NUMBER.")
